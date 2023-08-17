f014bf47ab427aa1f9e2b2742e94f12bb5964a8b
pechevarria <pechevarria05@gmail.com>
2023-08-17 01:41:16 -0500

    Lazy Loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './shared/pages/home-page/home-page.component';
 import { AboutPageComponent } from './shared/pages/about-page/about-page.component';
+import { ContactPageComponent } from './shared/pages/contact-page/contact-page.component';
 
 const routes: Routes = [
   {
@@ -13,8 +14,17 @@ const routes: Routes = [
     component: AboutPageComponent,
   },
   {
+    path: 'contact',
+    component: ContactPageComponent,
+  },
+  {
+    path: 'countries',
+    loadChildren: () =>
+      import('./countries/countries.module').then((m) => m.CountriesModule),
+  },
+  {
     path: '**',
-    redirectTo: 'home',
+    redirectTo: 'countries',
   },
 ];
 
